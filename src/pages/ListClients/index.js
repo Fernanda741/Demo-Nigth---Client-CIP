c548ed98e6c421b0de005fb3ef8ad8deb85c2e09
Taila Martins <tailamartins74@gmail.com>
2022-06-29 16:36:46 -0300

    feat/editable

--- src/pages/ListClients/index.js
+++ src/pages/ListClients/index.js
@@ -1,36 +1,47 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { Client } from "../../Components/Client";
-import { deleteClient, getClients } from "../../api/index";
-import { Modal } from "../../Components/Modal";
+import { deleteClient, getClients, updtadeClients } from "../../api/index";
+import { Modal, ModalEdit } from "../../Components/Modal";
 import { SearchBar } from "../../Components/SearchBar";
+import { Form } from "../../Components/Form";
 
 export const ListClients = () => {
-  const[clients, setClients] = useState([]);
-  const[modal,setModal] = useState(false);
-  const[deletingUser, setDeletingUser] = useState(-1);
+  const [clients, setClients] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [modalEdit, setModalEdit] = useState(false);
+  const [deletingUser, setDeletingUser] = useState(-1);
+  const [editClient, setEditClient] = useState("");
 
-
+  const clientEditData = clients.find((client) => client.id === editClient);
   useEffect(() => {
-   getClients()
-    .then((response) => response.json())
-    .then((data) => {
-      setClients(data.content);
-    })
-  },[]);
-  
+    getClients()
+      .then((response) => response.json())
+      .then((data) => {
+        setClients(data.content);
+      });
+  }, []);
+
   const handleDeleteClient = (id) => {
-    console.log(id)
-     deleteClient(id)
-     .then((response) => {
-      response.json()
-    })
-     
-     .then((data) => {
-       console.log(data, "dataaa")
-     })
+    console.log(id);
+    deleteClient(id)
+      .then((response) => {
+        response.json();
+      })
+
+      .then((data) => {
+        console.log(data, "dataaa");
+      });
   };
 
-  
+  const handleEditClient = (id, data) => {
+    updtadeClients(id, data).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      setModalEdit(true);
+    });
+  };
 
   return (
     <>
@@ -42,15 +53,18 @@ export const ListClients = () => {
             return (
               <div key={client.id}>
                 <Client
-                  client={client}                
+                  client={client}
                   onClickDelete={() => {
-                    setModal (true)
-                    setDeletingUser(client.id)
-                    }}
-                >                  
-                </Client>
-            </div>
-            );   
+                    setModal(true);
+                    setDeletingUser(client.id);
+                  }}
+                  onclickEdit={() => {
+                    setModalEdit(true);
+                    setEditClient(client.id);
+                  }}
+                ></Client>
+              </div>
+            );
           })}
         </ul>
         <Modal
@@ -61,7 +75,11 @@ export const ListClients = () => {
         >
           Você tem certeza que deseja excluir o cadastro do cliente?
         </Modal>
-       
+
+        <ModalEdit modal={modalEdit} onClickNo={() => setModalEdit(false)}>
+          <Form client={clientEditData} onSubmit={handleEditClient} />
+        </ModalEdit>
+
         {/* <ReactPaginate
           breakLabel="..."
           nextLabel="próxima >"
@@ -78,7 +96,6 @@ export const ListClients = () => {
           nextClassName="page-previous"
         /> */}
       </section>
-      
     </>
   );
 };
