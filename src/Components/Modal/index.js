1613b896e03280412ff43f7e1603e83314d3f72c
DevFranci <devfranciele@gmail.com>
2022-06-29 16:04:17 -0300

    WIP:css inicial do modal

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,22 +1,42 @@
-import "./styles.css";
-import Button  from "../Button";
+import './styles.css';
+import Button from '../Button';
 
 export const Modal = ({ children, click, onClickYes, onClickNo, modal }) => {
   return (
     <>
-      {modal && (
-        <div className="modal-container" onClick={click}>
-          <div className="modal">
-            <div className="modal-content">
-              <p>{children}</p>
-              <div className="modal-buttons">
-                <Button className="button-yes" onClick={onClickYes}>Sim</Button>
-                <Button className="button-no" onClick={onClickNo}>Não</Button>
-              </div>
+      <div
+        className={(modal ? 'active' : '') + ' modal-container'}
+        onClick={click}
+      >
+        <form>
+          <section id="post" className="post">
+            <div className="post-container">
+              <h2>New Post</h2>
+            </div>
+          </section>
+          <div className="container-btns">
+            <button id="btn-post" className="btn btn-secondary">
+              Post
+            </button>
+            <button id="btn-cancel" className="btn btn-primary">
+              Cancel
+            </button>
+          </div>
+        </form>
+        <div className="modal">
+          <div className="modal-content">
+            <p>{children}</p>
+            <div className="modal-buttons">
+              <Button className="button-yes" onClick={onClickYes}>
+                Sim
+              </Button>
+              <Button className="button-no" onClick={onClickNo}>
+                Não
+              </Button>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
