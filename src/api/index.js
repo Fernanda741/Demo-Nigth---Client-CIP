85fe46d4a0f56206fd6a6bd71c817e9ee6349d73
Taila Martins <tailamartins74@gmail.com>
2022-06-28 21:06:30 -0300

    fix/link-api

--- src/api/index.js
+++ src/api/index.js
@@ -5,18 +5,21 @@ export const CreateUser = (data) => {
   data.cep = data.cep.replace("-", "");
   data.codigo = faker.random.alphaNumeric(10);
 
-  return fetch("https://bank-services-challenge.herokuapp.com/api/v1/cliente", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+  return fetch(
+    "https://bank-services-challenge2.herokuapp.com/api/v1/cliente",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
 };
 
 export const getClients = () => {
-  return fetch("https://bank-services-challenge.herokuapp.com/api/v1/cliente",{
+  return fetch("https://bank-services-challenge.herokuapp.com/api/v1/cliente", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-    }
-  })
+    },
+  });
 };
